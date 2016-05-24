f61ab137682b439773b37886b105d489fecdbc94
Jonathan <carpenter.prime@gmail.com>
2016-05-24 13:31:46 -0400

    Removed width/height variables

--- CrazyPong/CrazyPong/CrazyPong.py
+++ CrazyPong/CrazyPong/CrazyPong.py
@@ -2,7 +2,5 @@ from cocos.director import director
 from cocos.scene import Scene
 from mylayers import GameLayer
 
-width = 800
-height = 800
 director.init(800, 800)
 director.run(Scene(GameLayer()))
