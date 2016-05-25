6dc9df80f4a1a94d056488a16725cc4d13453ae7
Jonathan <carpenter.prime@gmail.com>
2016-05-25 06:58:24 -0400

    Changed Screen Size

--- CrazyPong/CrazyPong/CrazyPong.py
+++ CrazyPong/CrazyPong/CrazyPong.py
@@ -2,5 +2,5 @@ from cocos.director import director
 from cocos.scene import Scene
 from mylayers import GameLayer
 
-director.init(800, 800)
+director.init(800, 600)
 director.run(Scene(GameLayer()))
